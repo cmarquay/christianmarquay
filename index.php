11c23bd38e2523fb195cf1787c9e5d58baa39a5c
Christian MARQUAY <cmarquay@etudiant.univ-lr.fr>
2015-12-25 23:36:06 +0100

    Mise en place du logo du site

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Christian MARQUAY</title>
+        <meta name="description" content="Le site de Christian MARQUAY, par Christian MARQUAY, pour Christian MARQUAY">
+        <meta name="robots" content="index, follow">
+        <meta name="author" content="Christian MARQUAY"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" href="images/cm1.png" />
+        <link rel="stylesheet" href="css/style.min.css" />
+    </head>
+    <body>
+        <header>
+            <h1>
+                <a href="index.php"><span>CM</span> Christian MARQUAY</a>
+            </h1>
+        </header>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.js" type="text/javascript"></script>
+        <script src="js/comportement.min.js" type="text/javascript"></script>
+    </body>
+</html>
---
+++ js/comportement.js
@@ -0,0 +1,5 @@
+$(document).ready(init);
+
+function init() {
+    alert("Site en construction ...\n\nMerci d'être passé !\n\n:-)\n\n");
+}
