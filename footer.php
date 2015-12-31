42d9dab642686064e1935f94aa8026aa869ba9fa
Christian MARQUAY <cmarquay@etudiant.univ-lr.fr>
2015-12-31 18:51:10 +0100

    Plus de footer

--- footer.php
+++ footer.php
@@ -5,8 +5,12 @@
         <p><a href="https://www.google.com/maps/place/78+Rue+Lucile,+17000+La+Rochelle,+France/@46.139735,-1.1709032,17z/data=!3m1!4b1!4m2!3m1!1s0x480153eef26a6f47:0x37bf59cfd673d2e2" target="_blank">78 rue Lucile, 17000 La Rochelle, France</a></p>
     </section>
     <section>
-        <p><a href="https://twitter.com/cmarquay" target="_blank">a</a></p>
-        <p><a href="https://www.facebook.com/christian.marquay" target="_blank">b</a></p>
-        <p><a href="https://www.linkedin.com/in/christian-marquay-8903799a" target="_blank">j</a></p>
+        <ul>
+            <li><a href="https://twitter.com/cmarquay" target="_blank">a</a></li>
+            <li><a href="https://github.com/cmarquay" target="_blank">Q</a></li>
+            <li><a href="https://www.facebook.com/christian.marquay" target="_blank">b</a></li>
+        </ul>
     </section>
+    <div class="trapeze"><p>En savoir plus ?</p></div>
+    <div class="pointe"></div>
 </footer>
