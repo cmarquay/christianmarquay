5b08e2ea93185e5a6757d4e31ad0cc6384ddd9a6
cmarquay <christianmarquay@hotmail.com>
2018-05-27 13:27:24 +0200

    update contact form

--- contact.php
+++ contact.php
@@ -1,54 +1,54 @@
 <?php
-    include_once("utile.php");
-    $titre = "Contact";
-    $description = "Contacter Christian MARQUAY, par messagerie interposée, par les réseaux sociaux, ou par un autre site web.";
-    $h2 = "Email et réseaux sociaux";
-    $affichage = "";
-    if(isset($_POST["send"])) {
-        if(isset($_POST["nom"]) && isset($_POST["email"]) && isset($_POST["sujet"]) && isset($_POST["message"]) &&
-            !empty($_POST["nom"]) && !empty($_POST["email"]) && !empty($_POST["sujet"]) && !empty($_POST["message"])) {
-            $email = addslashes(htmlentities($_POST["email"]));
-            if(VerifierAdresseMail($email)) {
-                $nom = addslashes(htmlentities($_POST["nom"]));
-                $headers = "MIME-Version: 1.0\r\n";
-                $headers .= "Content-type: text/html; charset=iso-8859-1";
-                $sujet = addslashes(htmlentities($_POST["sujet"]));
-                $message = array(addslashes(htmlentities($_POST["message"])),addslashes(htmlentities("\" a bien été envoyé à Christian MARQUAY.")),addslashes(htmlentities("Merci de ne pas répondre à ce courriel.")));
-                $message1 = "<html><head><title>Message provenant du formulaire de www.christianmarquay.com</title></head>
-                        <body><p>".$message[0]."</p></body></html>";
-                $message2 = "<html><head><title>Confirmation d'envoi du message sur le formulaire du site www.christianmarquay.com</title></head>
-                        <body><p>Bonjour,</p><p>Le message : \"".$message[0].$message[1]."</p><p>".$message[2]."</p></body></html>";
-                mail($email,"Confirmation d'envoi",$message2,$headers);
-                $headers .= "\r\nFrom: ".$nom." <".$email.">";
-                mail("cmarquay@etudiant.univ-lr.fr",$sujet,$message1,$headers);
-                $affichage = "Votre message a bien été envoyé, vous allez recevoir une confirmation par email.";
-            } else {
-                $affichage = "Adresse email incorrecte !";
-            }
+include_once("utile.php");
+$titre = "Contact";
+$description = "Contacter Christian MARQUAY, par messagerie interposée, par les réseaux sociaux, ou par un autre site web.";
+$h2 = "Email et réseaux sociaux";
+$affichage = "";
+if (isset($_POST["send"])) {
+    if (isset($_POST["nom"]) && isset($_POST["email"]) && isset($_POST["sujet"]) && isset($_POST["message"]) &&
+        !empty($_POST["nom"]) && !empty($_POST["email"]) && !empty($_POST["sujet"]) && !empty($_POST["message"])) {
+        $email = addslashes(htmlentities($_POST["email"]));
+        if (VerifierAdresseMail($email)) {
+            $nom = addslashes(htmlentities($_POST["nom"]));
+            $headers = "MIME-Version: 1.0\r\n";
+            $headers .= "Content-type: text/html; charset=iso-8859-1";
+            $sujet = "Message provenant du formulaire en ligne : " . addslashes(htmlentities($_POST["sujet"]));
+            $message = array(addslashes(htmlentities($_POST["message"])), addslashes(htmlentities("\" a bien été envoyé à Christian MARQUAY.")), addslashes(htmlentities("Merci de ne pas répondre à ce courriel.")));
+            $message1 = "<html><head><title>Message provenant du formulaire de www.christianmarquay.com</title></head>
+                        <body><p>" . $message[0] . "</p></body></html>";
+            $message2 = "<html><head><title>Confirmation d'envoi du message sur le formulaire du site www.christianmarquay.com</title></head>
+                        <body><p>Bonjour,</p><p>Le message : \"" . $message[0] . $message[1] . "</p><p>" . $message[2] . "</p></body></html>";
+            mail($email, "Confirmation d'envoi", $message2, $headers);
+            $headers .= "\r\nFrom: " . $nom . " <" . $email . ">";
+            mail("Christianmarquay@hotmail.com", $sujet, $message1, $headers);
+            $affichage = "Votre message a bien été envoyé, vous allez recevoir une confirmation par email.";
         } else {
-            echo "<script type=\"text/javascript\">"; //traitement javascript
-            echo "alert(\"Complétez tous les champs\");";
-            echo "window.history.back();";
-            echo "</script>";
+            $affichage = "Adresse email incorrecte !";
         }
+    } else {
+        echo "<script type=\"text/javascript\">";  //traitement javascript
+        echo "alert(\"Complétez tous les champs\");";
+        echo "window.history.back();";
+        echo "</script>";
     }
-    include_once("header.php");
+}
+include_once("header.php");
 ?>
     <main>
         <section class="form">
             <h3>CONTACT</h3>
             <p>Pour tous commentaires, demandes, questions, et retours, merci d'utiliser le formulaire suivant.</p>
-            <form id="formulaire" enctype="multipart/form-data" method="post" action="contact.php">
+            <form id="formulaire" method="post" action="contact.php">
                 <p><label for="edtNom" id="idNom">NOM</label></p>
-                <p><input type="text" id="edtNom" name="nom" required /></p>
+                <p><input type="text" id="edtNom" name="nom" required/></p>
                 <p><label for="edtEmail" id="idEmail">ADRESSE EMAIL</label></p>
-                <p><input type="email" id="edtEmail" name="email" required /></p>
+                <p><input type="email" id="edtEmail" name="email" required/></p>
                 <p><label for="edtSujet" id="idSujet">SUJET</label></p>
-                <p><input type="text" id="edtSujet" name="sujet" required /></p>
+                <p><input type="text" id="edtSujet" name="sujet" required/></p>
                 <p><label for="edtMessage" id="idMessage">MESSAGE</label></p>
-                <p><textarea id="edtMessage" name="message" required ></textarea></p>
-                <p class="submit"><input type="submit" id="btnSubmit" name="send" value="Envoyer le message" /></p>
-                <?php echo "<p>".$affichage."</p>"; ?>
+                <p><textarea id="edtMessage" name="message" required></textarea></p>
+                <p class="submit"><input type="submit" id="btnSubmit" name="send" value="Envoyer le message"/></p>
+                <?php echo "<p>" . $affichage . "</p>"; ?>
             </form>
         </section>
         <section>
@@ -62,4 +62,4 @@
             <p><a href="https://github.com/cmarquay" target="_blank">Q</a></p>
         </section>
     </main>
-    <?php include_once("footer.php"); ?>
+<?php include_once("footer.php"); ?>
--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Christian MARQUAY</title>
-        <meta name="description" content="Le site de Christian MARQUAY, par Christian MARQUAY, pour Christian MARQUAY.">
-        <meta name="robots" content="index, follow">
-        <meta name="author" content="Christian MARQUAY"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/png" href="images/cm.png" />
-        <link rel="stylesheet" href="js/leaflet/leaflet.css" />
-        <link rel="stylesheet" href="css/style.min.css" />
-    </head>
-    <body class="home">
-    <?php include_once("analyticstracking.php") ?>
-    <header>
-        <h1><a href="index.php"><span>CM</span> Christian MARQUAY</a></h1>
-        <nav>
-            <ul>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="contact.php">CONTACT</a></li>
-            </ul>
-        </nav>
-    </header>
-    <main>
+<head>
+    <meta charset="utf-8"/>
+    <title>Christian MARQUAY</title>
+    <meta name="description" content="Le site de Christian MARQUAY, par Christian MARQUAY, pour Christian MARQUAY.">
+    <meta name="robots" content="index, follow">
+    <meta name="author" content="Christian MARQUAY"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="icon" type="image/png" href="images/cm.png"/>
+    <link rel="stylesheet" href="js/leaflet/leaflet.css"/>
+    <link rel="stylesheet" href="css/style.min.css"/>
+</head>
+<body class="home">
+<?php include_once("analyticstracking.php") ?>
+<header>
+    <h1><a href="index.php"><span>CM</span> Christian MARQUAY</a></h1>
+    <nav>
+        <ul>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="contact.php">CONTACT</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
 
-    </main>
-    <?php include_once("footer.php"); ?>
+</main>
+<?php include_once("footer.php"); ?>
